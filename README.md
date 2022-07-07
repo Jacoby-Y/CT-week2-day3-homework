93089930a82365e06e4f26196630b528b627d133
Jacoby Yliniemi <cobyyliniemi@gmail.com>
2022-07-07 16:22:14 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Coding Temple Week 2 Day 3 Homework
+
+###### CT-week2-day3-homework
---
+++ mod.py
@@ -0,0 +1,7 @@
+from math import pi
+
+def calc_area(width, height):
+	return width * height
+
+def get_circumference(radius):
+	return 2 * pi * radius
